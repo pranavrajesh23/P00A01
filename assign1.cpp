ab0a2dc1b9e861c473688a771916a8c3157ee365
pranavrajesh23 <pranav.rajesh@psiog.com>
2025-08-14 14:35:08 +0530

    Created and data are appended

--- assign1.cpp
+++ assign1.cpp
@@ -5,5 +5,22 @@ int main()
 {
     fstream fout;
     fout.open("one.csv",ios::out | ios::app);
-    cout<<"CSV file created successfully";
+    cout<<"CSV file created successfully"<<endl;
+
+    int n;
+    cout<<"Enter the no. of rows required"<<endl;
+    cin>>n;
+
+    fout<<"Name,Age,Place\n";
+    cout<<"Enter the details"<<endl;
+    
+    int age;
+    string name,place;
+    
+    for(int i=0;i<n;i++)
+    {
+        cin>>name>>age>>place;
+        fout<<name<<","<<age<<","<<place<<"\n";
+    }
+    cout<<"CSV file is created and data are appended successfully";
 }
