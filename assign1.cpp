ed16a5efde8a44931e06b00e1834afca9bb46558
pranavrajesh23 <pranav.rajesh@psiog.com>
2025-08-15 13:20:12 +0530

    Test case check stating the existence of data in destination file and make check on the same data on both source and destination file

--- assign1.cpp
+++ assign1.cpp
@@ -28,34 +28,69 @@ void copyingData(const string &source, const string &destination)
     }
     src.close();
     dest.close();
+    cout<<"Copied successfully.\n";
+}
+bool similarityCheck(const string &source,const string &destination)
+{
+    fstream src, dest;
+    src.open(source,ios::in);
+    dest.open(destination,ios::in);
+    string one,two;
+    bool flag=true;
+    while(getline(src,one) && getline(dest,two))
+    {
+        if(one!=two)
+        {
+            flag=false;
+            break;
+        }
+    }
+    src.close();
+    dest.close();
+    return flag;
 }
 int main()
 {
     
     if(!fileDoesExist(source))
     {
-        cout<<"Source file does not exist by default";
+        cout<<"Source file does not exist by default.\n";
     }
     else
     {
         
             if(fileContainData(source))
-            {
-                cout<<"Coping source file to destination file\n";
+            { 
                 if(!fileDoesExist(destination))
                 {
-                    cout<<"Destination file does not exist by default, so creating destination file to copy things from source file\n";
+                    cout<<"Destination file does not exist by default, so creating destination file to copy things from source file.\n";
                     createCsvFile();
+                    cout<<"Coping source file to destination file.\n";
                     copyingData(source,destination);
                 } 
-                copyingData(source,destination);
-                cout<<"Copied data from source to destination\n";
+                else
+                {
+                    if(fileContainData(destination))
+                    {
+                        cout<<"Since the destination file does contain few data, we checking for the existence of same data in both files.\n";
+                        if(similarityCheck(source,destination))
+                        {
+                            cout<<"Both files have same data, so no need to copy data from source file.\n";
+                        }
+                        else
+                        {
+                            cout<<"Coping source file to destination file.\n";
+                            copyingData(source,destination);
+                        }
+                    }
+                }
             }
             else
             {
-                cout<<"Source file exists but it dosen't contain any data such that there is no data to copy\n";
+                cout<<"Source file exists but it dosen't contain any data such that there is no data to copy.\n";
                 fstream file(destination,ios::out | ios::trunc);
                 file.close();
             }
     }
+    return 0;
 }
